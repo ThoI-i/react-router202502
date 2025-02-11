42a6811aaeab6e4cd3175e16782245827b3a7f71
Theo <ojw1912@gmail.com>
2025-02-11 11:31:11 +0900

    250211 11:30 # 2. Link 사용하기

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,24 +1,15 @@
-import { Link } from 'react-router-dom';
+import MainNav from '../components/MainNav';
 import styles from './IndexPage.module.scss';
 
 const IndexPage = () => {
   return (
-    <div className={styles.home}>
-      <h1 className={styles.title}>개발자의 기술 블로그</h1>
-      <p className={styles.subtitle}>React와 관련된 기술들을 공유합니다.</p>
-
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/blog'>Blog</Link>
-        </li>
-        <li>
-          <Link to='/about'>About</Link>
-        </li>
-      </ul>
-    </div>
+    <>
+      <MainNav />
+      <div className={styles.home}>
+        <h1 className={styles.title}>개발자의 기술 블로그</h1>
+        <p className={styles.subtitle}>React와 관련된 기술들을 공유합니다.</p>
+      </div>
+    </>
   );
 };
 
