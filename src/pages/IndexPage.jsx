08bbc6e1dbffdabab78e17c7797ad372e0a17222
Theo <ojw1912@gmail.com>
2025-02-11 11:19:12 +0900

    250211 11:15 # 2-0-A. a태그를 통한 라우터(페이지 깜박임/모든 상태값 초기화)

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,5 +1,24 @@
+import styles from './IndexPage.module.scss';
+
 const IndexPage = () => {
-  return <h1>IndexPage입니다.</h1>;
+  return (
+    <div className={styles.home}>
+      <h1 className={styles.title}>개발자의 기술 블로그</h1>
+      <p className={styles.subtitle}>React와 관련된 기술들을 공유합니다.</p>
+
+      <ul>
+        <li>
+          <a href='/'>Home</a>
+        </li>
+        <li>
+          <a href='/blog'>Blog</a>
+        </li>
+        <li>
+          <a href='/about'>About</a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default IndexPage;
